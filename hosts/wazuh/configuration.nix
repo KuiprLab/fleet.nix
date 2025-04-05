c21f731b6373ac4f5fe347dfcfbcc6b59555d511
daniel <daniel.inama02@gmail.com>
2025-04-05 14:26:27 +0200

    chore: automatic commit before deployment

--- hosts/wazuh/configuration.nix
+++ hosts/wazuh/configuration.nix
@@ -1,8 +1,16 @@
-_:{
+{modulesPath,...}:{
+
+  imports = [
+    (modulesPath + "/virtualisation/proxmox-lxc.nix")
+  ];
   programs.wazuh = {
         enable = true;
     username = "daniel";
         hashedPassword = "$2a$10$CvmkwWOLj0ThTVnKLEM5neZOWS5GZ7cZQBIXvL/fs6keSnU15C/DG";
     };
 
+      proxmoxLXC = {
+        manageNetwork = false;
+        privileged = false;
+      };
 }
